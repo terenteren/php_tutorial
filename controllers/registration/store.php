edcfe4fc49105b7f8132ef326db865d98f728e5a
terenteren <jaewon8752@gmail.com>
2024-07-26 14:53:09 +0900

    Password encryption using PASSWORD_BCRYPT

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -1,13 +1,11 @@
 <?php
 
-
 use Core\App;
 use Core\Database;
 use Core\Validator;
 
 $email = $_POST['email'];
 $password = $_POST['password'];
-
 
 // validate the form inputs.
 $errors = [];
@@ -26,26 +24,22 @@ if (! empty($errors)) {
 }
 
 $db = App::resolve(Database::class);
-// check if the account already exists
+
 $user = $db->query('select * from users where email = :email', [
     'email' => $email
 ])->find();
 
 if ($user) {
-    // then someone with that email already exists and has on account.
-    // If yes, redirect to a login page.
     header('Location: /login');
     exit();
 } else {
-    // If not, save one to the database, and then log the user in, and redirect.
     $name = explode('@', $email)[0];
     $db->query('INSERT INTO users (email, name, password) VALUES (:email, :name, :password)', [
         'email' => $email,
         'name' => $name,
-        'password' => password_hash($password, PASSWORD_DEFAULT)
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
 
-    // mark that the user has logged in.
     $_SESSION['user'] = [
         'email' => $email
     ];
