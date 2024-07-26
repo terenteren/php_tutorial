c3b7db4240864177e7d60d8041ad340fa86fc6e7
terenteren <jaewon8752@gmail.com>
2024-07-26 16:54:57 +0900

    Extract an Authenticator Class

--- Core/functions.php
+++ Core/functions.php
@@ -40,20 +40,8 @@ function view($path, $attributes = [])
     require base_path("views/{$path}.view.php");
 }
 
-function login($user)
+function redirect($path)
 {
-    $_SESSION['user'] = [
-        'email' => $user['email']
-    ];
-
-    session_regenerate_id(true);
-}
-
-function logout()
-{
-    $_SESSION = [];
-    session_destroy();
-
-    $params = session_get_cookie_params();
-    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+    header("Location: {$path}");
+    exit();
 }
--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -25,4 +25,10 @@ class LoginForm
     {
         return $this->errors;
     }
+
+    public function error($field, $message)
+    {
+        $this->errors[$field] = $message;
+    }
+
 }
